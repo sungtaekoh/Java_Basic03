e961bdbc66203f0d1e3951f8d86943d3c7a9669c
brown <ohbass95@gmail.com>
2022-01-26 23:07:13 +0900

    10

--- day10_ClassVariable/Quiz01_1MainClass.java
+++ day10_ClassVariable/Quiz01_1MainClass.java
@@ -1,12 +1,19 @@
 package day10_ClassVariable;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
-
+/*
+ * 실패 
+ * 게임전적이 안나오는데 왜 안나오는지 모르겠음
+ * 
+ */
 public class Quiz01_1MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		Random rd = new Random(); 
+		ArrayList a = new ArrayList();
+		int cnt = 0;
 		int answer = 0;
 		
 		while(true) {
@@ -22,17 +29,31 @@ public class Quiz01_1MainClass {
 			while(true) {
 			System.out.println("Input Number --> ");
 			answer = input.nextInt();
-			if(num > answer) {
-				System.out.println("=== Up ===");
-			}else if(num < answer) {
-				System.out.println("=== Down ===");
-			}else {
-				System.out.println("정답입니다");
+			if(answer == num) {
+				cnt++;
+				a.add(cnt);
+				System.out.println(cnt+"회 만에 맞췄습니다!!");
+				cnt = 0;
 				break;
+			}else if(answer < num) {
+				cnt++;
+				System.out.println("=== Up ===");
+			}else if(answer > num) {
+				cnt++;
+				System.out.println("=== Down ===");
 			}
 			}
 		}else if(choice ==2) {//게임 전적
-			
+			int score = 0;
+			score = (int)a.get(0);
+			for(int i =1; i<a.size();i++) {
+				if(score < (int)a.get(i)) {
+					score = (int)a.get(i);
+				}else {
+					
+				}
+			}
+			System.out.println("최고 점수는 "+score+"회 입니다!!!");
 		}else if(choice == 3) {//게임 종료
 			break;
 		}
