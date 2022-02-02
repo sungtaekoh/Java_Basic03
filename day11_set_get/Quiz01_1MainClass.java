900b853f37a5038ab7950c35bee4204847f08160
brown <ohbass95@gmail.com>
2022-02-03 00:55:01 +0900

    D15-1

--- day11_set_get/Quiz01_1MainClass.java
+++ day11_set_get/Quiz01_1MainClass.java
@@ -7,23 +7,29 @@ import java.util.Scanner;
  -클래스 형태로 기능별로 만들것
  -this,get,set 활용해서 만들것
  */
+/*
+  작동은 잘 되는데 이렇게 하는게 맞는지 모르겠다
+  변수 설정을 private으로 설정했는데 왜 접근이 가능한걸까
+  set을 사용하지 않았음에도 말이다
+ */
 public class Quiz01_1MainClass {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		 Quiz01_1Method ex = new  Quiz01_1Method ();
 		boolean bool =true;
 		int choice = 0;
 		
 		while(bool) {
-		System.out.print("1. 로그인");
-		System.out.print("2. 가입");
-		System.out.print("3. 로그아웃");
+		System.out.print("1.로그인  ");
+		System.out.print("2.가입	");
+		System.out.print("3.로그아웃	");
 		choice = input.nextInt();
 		if(choice == 1) {
-			
+			ex.login();
 		}else if(choice ==2) {
-			
+			ex.newLog();
 		}else if(choice ==3) {
-			
+			bool = ex.logout(false);
 		}else {
 			System.out.println("잘 못 된 선택입니다");
 		}
--- day11_set_get/Quiz01_1Method.java
+++ day11_set_get/Quiz01_1Method.java
@@ -5,22 +5,69 @@ import java.util.Scanner;
 
 public class Quiz01_1Method {
 	Scanner input = new Scanner(System.in);
-		ArrayList saveId = new ArrayList();
-		ArrayList savePw = new ArrayList();
+		private ArrayList saveId = new ArrayList();
+		private ArrayList savePw = new ArrayList();
 		
-		String id = null;
-		String pw = null;
-	public void newcheck() {
-		System.out.println("아이디 입력 : ");
-		id = input.next();
-		if(saveId.indexOf(id) == -1) {
-		System.out.println("비밀번호 입력 : ");
-		pw = input.next();
-		saveId.add(id);
-		savePw.add(pw);
-		System.out.println("회원가입에 성공하셧습니다");
+		private String id = null;
+		private String pw = null;
+		
+		public void setId(String id) {
+			this.id = id;
 		}
-	}
-	
+		
+		public void setPw(String pw) {
+			this.pw = pw;
+		}
+		public void setSaveId(ArrayList saveId) {
+			this.saveId = saveId;
+		}
+		public void setSavePw(ArrayList savePw) {
+			this.savePw = savePw;
+		}
+		
+		public String getId() {
+			return id;
+		}
+		
+		public String getPw() {
+			return pw;
+		}
+		public ArrayList getSaveId() {
+			return saveId;
+		}
+		public ArrayList getSavePw() {
+			return savePw;
+		}
+		
+		
+		public void login() {
+			System.out.println("아이디 입력 : ");
+			id = input.next();
+			System.out.println("비밀번호 입력 :");
+			pw = input.next();
+			if(saveId.indexOf(id) != -1 && savePw.indexOf(pw) != -1){
+				System.out.println("로그인에 성공하셨습니다.");
+			}else {
+				System.out.println("로그인에 실패하셨습니다.");
+			}
+		}
+		
+		public void newLog() {//회원가입
+			System.out.println("아이디 입력 : ");
+			String id = input.next();
+			if(saveId.indexOf(id) == -1) {
+			System.out.println("비밀번호 입력 : ");
+			String pw = input.next();
+			saveId.add(id);
+			savePw.add(pw);
+			System.out.println("회원가입 되셨습니다.");
+			}else {
+				System.out.println("이미 존재하는 아이디입니다");
+			}
+		}
+		public boolean logout(boolean bool) {
+			System.out.println("로그아웃 합니다");
+			return bool;
+		}
 	
 }
