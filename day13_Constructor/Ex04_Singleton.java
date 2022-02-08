64964213b327a39f8baddc9ac31a3c86443674c7
brown <ohbass95@gmail.com>
2022-02-08 21:19:03 +0900

    D19-2

--- day13_Constructor/Ex04_Singleton.java
+++ day13_Constructor/Ex04_Singleton.java
@@ -14,25 +14,39 @@ class Singleton04{
 	public static Singleton04 getInstance() {
 		//생성자가 static으로 만들어져 있어 호출 가능
 		
-		si = new Singleton04();
-		//메소드를 통해 객체를 만든다. new가 아니면 객체가 생성되지 않는다.
+//		si = new Singleton04();
+//메소드를 통해 새로운 객체를 만든다. new가 아니면 객체가 생성되지 않는다.
 		
 		System.out.println("si : "+si);
-		
-		
-		return si;
+		if(si == null) 
+			si = new Singleton04();
+			
+			return si;
 	}
 	
 }
 
 public class Ex04_Singleton {
 	public static void main(String[] args) {
+		/*
+		 개념정리 : 
+		 Singleton : 객체를 하나만 만들어서 사용한다.
+		 */
+		
 //		Calendar cal = Calendar.getInstance();
 		
 		Singleton04 si01 = Singleton04.getInstance();
 		//new가 아닌 객체를 얻어 와서 사용한다.
 		
 		System.out.println("si01 : "+si01);
+		
+		Singleton04 si02 = Singleton04.getInstance();
+		System.out.println("si02 : "+si02);
+		Singleton04 si03 = Singleton04.getInstance();
+		System.out.println("si02 : "+si03);
+		Singleton04 si04 = Singleton04.getInstance();
+		System.out.println("si02 : "+si04);
+//		si 02, 03, 04... 전부 같은 값이 나오게 된다.
 		
 	}
 }
---
+++ day13_Constructor/Ex05_Inheritance.java
@@ -0,0 +1,5 @@
+package day13_Constructor;
+
+public class Ex05_Inheritance {
+
+}
